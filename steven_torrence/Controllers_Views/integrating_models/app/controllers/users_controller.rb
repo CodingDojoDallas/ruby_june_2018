ac8499771bbc4090a0740e171e94c65c70600a8c
Steven Torrence <steven@Stevens-MacBook-Pro.local>
2018-06-10 23:17:37 -0500

    Added Integrating Models Assignment

---
+++ steven_torrence/Controllers_Views/integrating_models/app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+
+    def new
+        render "new"
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def edit
+        @user = User.find(params[:id])
+        render "edit"
+    end
+
+    def create
+		@new_user = User.create(user_params)
+		redirect_to '/users'
+    end
+    
+    def update
+		User.update(params[:id], user_params)
+		redirect_to '/users'
+	end
+    
+    def total
+        @user_total = User.all.count
+        render text: "There are a total of #{@user_total} users!"
+    end
+
+    private
+		def user_params
+			params.require(:user).permit(:id, :first_name, :last_name, :created_at, :updated_at)
+		end
+end
