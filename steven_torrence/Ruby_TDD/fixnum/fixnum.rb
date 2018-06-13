7c84f38471346ba554524c86ec8322729ffa58b4
Steven Torrence <steven@Stevens-MacBook-Pro.local>
2018-06-13 16:10:06 -0500

    Created new TDD folder and Added Project Assignment

---
+++ steven_torrence/Ruby_TDD/fixnum/fixnum.rb
@@ -0,0 +1,14 @@
+class Fixnum
+    # attr_accessor :fixnum1, :fixnum2, :fixnum3
+    def prev
+        self - 1
+    end
+
+    def double
+        self * 2
+    end
+
+    def skip
+        self + 5
+    end
+end
